a669741987a776dc66d8a6c094f394330fdb54df
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 14:54:12 +0530

    pattern 6 completed

--- pattern/pattern6.java
+++ pattern/pattern6.java
@@ -3,5 +3,14 @@ package pattern;
 public class pattern6 {
   public static void main(String[] args) {
     System.out.println("Pattern 6");
+    int n =5;
+    for(int i=0;i<n;i++)
+    {
+      for(int j=1;j<=n-i;j++)
+      {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
   }
 }
