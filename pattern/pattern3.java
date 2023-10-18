6e8a9cac56e5e1dae67c42a327427049854dfe9f
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 14:42:53 +0530

    pattern3 completed

--- pattern/pattern3.java
+++ pattern/pattern3.java
@@ -3,5 +3,14 @@ package pattern;
 public class pattern3 {
   public static void main(String[] args) {
     System.out.println("pattern 3 created");
+    int n = 5;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
   }
 }
