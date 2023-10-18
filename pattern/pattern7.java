9ab161ee706d541c6050534f05fa1fd6668d2ad4
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 15:22:36 +0530

    pattern 7 completed

--- pattern/pattern7.java
+++ pattern/pattern7.java
@@ -3,5 +3,22 @@ package pattern;
 public class pattern7 {
   public static void main(String[] args) {
     System.out.println("pattern 7");
+    int n = 5;
+    for (int m = 1; m <= n; m++) {
+      // left
+      for (int j = n; j > m; j--) {
+        System.out.print(" ");
+      }
+
+      // mid
+      for (int j = 1; j < m * 2; j++) {
+        System.out.print("*");
+      }
+      // right
+      for (int j = n; j > m; j--) {
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 }
