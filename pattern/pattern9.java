14f97ab9575e2831be129f5e626d743cefcac813
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 15:55:23 +0530

    pattern 9 top section completed

--- pattern/pattern9.java
+++ pattern/pattern9.java
@@ -1,7 +1,32 @@
 package pattern;
 
 public class pattern9 {
+   static void top(int n)
+  {
+   for(int i=0;i<n;i++)
+   {
+    //left
+    for(int j=0;j<n-i;j++)
+    {
+      System.out.print(" ");
+    }
+    //mid
+    for(int j=0;j<=i*2;j++)
+    {
+      System.out.print("*");
+    }
+    //right
+    for(int j=0;j<n-i;j++)
+    {
+      System.out.print(" ");
+    }
+    System.out.println();
+   }
+  }
   public static void main(String[] args) {
     System.out.println("Pattern 9");
+    int n = 5;
+    top(n);
+
   }
 }
