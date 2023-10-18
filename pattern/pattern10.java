1cc726086f15f4b8dce51584890ab702da603cf2
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 16:29:06 +0530

    pattern 10 top completed

--- pattern/pattern10.java
+++ pattern/pattern10.java
@@ -1,7 +1,20 @@
 package pattern;
 
 public class pattern10 {
+  static void top(int n)
+  {
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
   public static void main(String[] args) {
     System.out.println("Pattern 10");
+    int n = 5;
+    top(n);
   }
 }
