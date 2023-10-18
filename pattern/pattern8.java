293c0439cf3e232001e58030169b10e71f2ee8ae
saik434455@gmail.com <saik434455@gmail.com>
2023-10-18 15:46:00 +0530

    pattern 8 completed

--- pattern/pattern8.java
+++ pattern/pattern8.java
@@ -3,5 +3,25 @@ package pattern;
 public class pattern8 {
   public static void main(String[] args) {
     System.out.println("Pattern 8");
+    int n =5;
+    for(int i=0;i<n;i++)
+    {
+      //left
+      for(int j=0;j<i;j++)
+      {
+        System.out.print(" ");
+      }
+       //mid
+       for(int j=n*2-i-1;j>i;j--)
+       {
+        System.out.print("*");
+       }
+      //right
+      for(int j=0;j<i;j++)
+      {
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 }
