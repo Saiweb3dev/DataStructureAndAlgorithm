d3f4e62b796c74a382f44c9cf803f80bdf5eed57
saik434455@gmail.com <saik434455@gmail.com>
2023-10-19 15:09:39 +0530

    pattern 11 completed

--- pattern/pattern11.java
+++ pattern/pattern11.java
@@ -1,5 +1,16 @@
 package pattern;
 
 public class pattern11 {
-  
+    public static void main(String[] args) {
+        System.out.println("Pattern 11");
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            int val = i % 2 == 0 ? 0 : 1;
+            for(int j = 1; j <= i; j++) {
+                System.out.print(val);
+                val ^= 1;
+            }
+            System.out.println();
+        }
+    }
 }
