2161338653a1feb4d0549b44f2d51d69ed5d59af
saik434455@gmail.com <saik434455@gmail.com>
2023-10-19 17:00:32 +0530

    pattern 16 completed

---
+++ pattern/paatern16.java
@@ -0,0 +1,20 @@
+package pattern;
+
+public class paatern16 {
+  public static void main(String[] args) {
+    System.out.println("Pattern 16");
+    int n = 5;
+     char ch = 'A';
+    for(int i=0;i<n;i++)
+    {
+     
+      for(int j=0;j<=i;j++)
+      {
+        System.out.print(ch);
+       
+      }
+       ch++;
+      System.out.println();
+    }
+  }
+}
